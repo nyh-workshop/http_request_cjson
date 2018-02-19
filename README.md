61e9918ce4a70bdf1d052339296fbdc191aba49c
uncle-yong <The_YongGrand@yahoo.com>
2018-02-19 18:49:35 +0800

    First upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# HTTP Request with CJSON Example
+
+Uses a POSIX socket to make a very simple HTTP request.
+
+The HTTP request is performed on the OpenWeatherMap site and the JSON is obtained.
+
+Afterwards, the JSON information is parsed using CJSON. In this example, temperature and the weather status is 
+printed on the terminal.
+
+# For more information of installing the toolchain w/ Visual Studio code, proceed to the hackaday page:
+1. https://hackaday.io/project/53259-deploying-standalone-esp32-idf-sdk-platform (without PlatformIO)
+2. https://hackaday.io/project/43374-esp32-idf-deploying-the-development-platform (with PlatformIO)
---
+++ build/bootloader/soc/esp32/rtc_init.d
@@ -0,0 +1,38 @@
+esp32/rtc_init.o: C:/esp/esp-idf/components/soc/esp32/rtc_init.c \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ C:/esp/esp-idf/components/esp32/include/esp_assert.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/rtc.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/dport_reg.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/dport_access.h \
+ C:/esp/esp-idf/components/esp32/include/esp_attr.h \
+ C:/esp/esp-idf/components/esp32/include/esp_dport_access.h \
+ d:\esp32-projects\http_request\build\include\sdkconfig.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/efuse_reg.h \
+ C:/esp/esp-idf/components/soc/esp32/include/soc/gpio_reg.h
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+C:/esp/esp-idf/components/esp32/include/esp_assert.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/rtc.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/rtc_cntl_reg.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/dport_reg.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/dport_access.h:
+
+C:/esp/esp-idf/components/esp32/include/esp_attr.h:
+
+C:/esp/esp-idf/components/esp32/include/esp_dport_access.h:
+
+d:\esp32-projects\http_request\build\include\sdkconfig.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/efuse_reg.h:
+
+C:/esp/esp-idf/components/soc/esp32/include/soc/gpio_reg.h:
+/c/esp/esp-idf/components/soc/esp32/rtc_init.c:
